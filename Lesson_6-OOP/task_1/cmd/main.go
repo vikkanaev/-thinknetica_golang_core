49ca1ed31b83e3cb2b1ddbaffc5b2803e771b3a7
Viktor Kanaev <vikkanaev@gmail.com>
2022-05-10 16:39:53 +0400

    refactoring

--- Lesson_6-OOP/task_1/cmd/main.go
+++ Lesson_6-OOP/task_1/cmd/main.go
@@ -2,21 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"thinknetica_golang_core/Lesson_6-OOP/task_1/pkg/distance"
 )
 
 func main() {
-	p1, err := distance.NewPoint(1, 1)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	p2, err := distance.NewPoint(2, 2)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	d := distance.Calculate(p1, p2)
+	d := distance.CalculateDistance(1, 1, 4, 5)
 	fmt.Println(d)
 }
--- Lesson_6-OOP/task_1/pkg/distance/distance.go
+++ Lesson_6-OOP/task_1/pkg/distance/distance.go
@@ -1,25 +1,10 @@
 package distance
 
 import (
-	"errors"
 	"math"
 )
 
-type Point struct {
-	x, y float64
-}
-
-func NewPoint(x, y float64) (point Point, err error) {
-	if x < 0 || y < 0 {
-		err = errors.New("must be greater than 0")
-		return point, err
-	}
-	point.x = x
-	point.y = y
-	return point, err
-}
-
-func Calculate(p1, p2 Point) float64 {
-	distance := math.Sqrt(math.Pow(p2.x-p1.x, 2) + math.Pow(p2.y-p1.y, 2))
-	return distance
+// CalculateDistance возвращает растояние между двумя заданными точками
+func CalculateDistance(x1, y1, x2, y2 float64) float64 {
+	return math.Sqrt(math.Pow(x2-x1, 2) + math.Pow(y2-y1, 2))
 }
