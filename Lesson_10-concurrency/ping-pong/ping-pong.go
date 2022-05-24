86da55f98cfe73d1e27c468954fb3f6b244f95d6
Viktor Kanaev <vikkanaev@gmail.com>
2022-05-24 07:14:55 +0400

    move to one ch

--- Lesson_10-concurrency/ping-pong/ping-pong.go
+++ Lesson_10-concurrency/ping-pong/ping-pong.go
@@ -6,18 +6,18 @@ import (
 	"time"
 )
 
-func player(in <-chan string, out chan<- string, num int, wg *sync.WaitGroup) {
+func player(ch chan string, num int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	defer close(out)
 
 	var m = map[string]string{
 		"Ping": "Pong",
 		"Pong": "Ping",
 	}
 
-	for res := range in {
+	for res := range ch {
 		if res == "Stop" {
 			println("Player", num, "lose!")
+			close(ch)
 			return
 		}
 
@@ -25,7 +25,7 @@ func player(in <-chan string, out chan<- string, num int, wg *sync.WaitGroup) {
 		println("===>>>  Player", num, "see:", res, "reply", rep)
 		// Sleep добавлено исключительно для красоты отображение. Без него все работает так же
 		time.Sleep(time.Second)
-		out <- rep
+		ch <- rep
 	}
 	println("Player", num, "win!")
 }
@@ -40,26 +40,14 @@ func goal(str string) string {
 	return str
 }
 
-func start(ch1 chan<- string, ch2 chan<- string) {
-	rand.Seed(time.Now().UnixNano())
-	min := 1
-	max := 100
-	if (rand.Intn(max-min+1) + min) > 50 {
-		ch1 <- "Ping"
-	} else {
-		ch2 <- "Ping"
-	}
-}
-
 func main() {
-	ch1 := make(chan string)
-	ch2 := make(chan string)
+	ch := make(chan string)
 	var wg sync.WaitGroup
 
 	wg.Add(2)
-	go player(ch1, ch2, 1, &wg)
-	go player(ch2, ch1, 2, &wg)
+	go player(ch, 1, &wg)
+	go player(ch, 2, &wg)
 
-	start(ch1, ch2)
+	ch <- "Ping"
 	wg.Wait()
 }
