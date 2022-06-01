f090aeb6c50088eb5ff3db9dbdfb2829df71cdea
Viktor Kanaev <vikkanaev@gmail.com>
2022-06-01 12:16:23 +0400

    review based changes

--- Lesson_12-web/pkg/webapp/webapp.go
+++ Lesson_12-web/pkg/webapp/webapp.go
@@ -9,38 +9,23 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Interface interface {
-	Start(*index.Index, []crawler.Document)
-}
-
 type Webapp struct {
-	addr string
 	i    index.Index
 	docs []crawler.Document
 }
 
-// Создает новый объект веб-сервера и возвращает указатель на него
-func New() *Webapp {
-	var w Webapp
-	w.addr = ":80"
-	return &w
-}
-
 // Запускает веб-сервер
-func (wa *Webapp) Start(ind *index.Index, d []crawler.Document) {
+func New(r *mux.Router, ind *index.Index, d []crawler.Document) {
+	var wa Webapp
 	wa.i = *ind
 	wa.docs = d
-	mux := mux.NewRouter()
-	mux.HandleFunc("/index", wa.indexHandler).Methods(http.MethodGet)
-	mux.HandleFunc("/docs", wa.docsHandler).Methods(http.MethodGet)
-
-	http.ListenAndServe(wa.addr, mux)
+	r.HandleFunc("/index", wa.indexHandler).Methods(http.MethodGet)
+	r.HandleFunc("/docs", wa.docsHandler).Methods(http.MethodGet)
 }
 
 // HTTP-обработчик для эндпоинта /index
 func (wa *Webapp) indexHandler(w http.ResponseWriter, r *http.Request) {
 	responser(w, "Index", formatArr(wa.i.ToStrs()))
-
 }
 
 // HTTP-обработчик для эндпоинта /docs
@@ -54,7 +39,6 @@ func (wa *Webapp) docsHandler(w http.ResponseWriter, r *http.Request) {
 
 // Формирует ответ пользователю
 func responser(w http.ResponseWriter, title string, data string) {
-	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "<html><body><h2>%v</h2><div>%v</div></body></html>", title, data)
 }
 
