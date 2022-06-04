e1f2dd2144c34c8a7c5e2b24a063c1c100779876
Viktor Kanaev <vikkanaev@gmail.com>
2022-06-04 20:57:24 +0400

    hw 14 gRPC

---
+++ Lesson_14-RPC/server/server.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"sync"
+	pb "thinknetica_golang_core/Lesson_14-RPC/messages_proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type Signalman struct {
+	Data []pb.Message
+	mu   sync.Mutex
+
+	// Композиция интерфейса.
+	pb.SignalmanServer
+}
+
+func (s *Signalman) Messages(_ *pb.Empty, stream pb.Signalman_MessagesServer) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	for _, item := range s.Data {
+		stream.Send(&item)
+	}
+	return nil
+}
+
+func (s *Signalman) Send(_ context.Context, message *pb.Message) (*pb.Empty, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.Data = append(s.Data, *message)
+	return nil, nil
+}
+
+func main() {
+	log.Println("Server starting")
+	srv := Signalman{}
+	messages1 := pb.Message{Id: 1, Text: "The Go Programming Language", CreatedAt: timestamppb.Now()}
+	messages2 := pb.Message{Id: 2, Text: "1984", CreatedAt: timestamppb.Now()}
+	srv.Data = append(srv.Data, messages1, messages2)
+
+	lis, err := net.Listen("tcp", ":12345")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSignalmanServer(grpcServer, &srv)
+	grpcServer.Serve(lis)
+}
--- go.mod
+++ go.mod
@@ -7,4 +7,11 @@ require golang.org/x/net v0.0.0-20220403103023-749bd193bc2b
 require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	golang.org/x/exp v0.0.0-20220602145555-4a0574d9293f // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/grpc v1.47.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
