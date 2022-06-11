7cd89448b9416a94f350413f09aba145a0316bbd
Viktor Kanaev <vikkanaev@gmail.com>
2022-06-11 14:23:00 +0400

    Add storage

--- Lesson_19-queue/1-Shortener/cmd/main.go
+++ Lesson_19-queue/1-Shortener/cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"thinknetica_golang_core/Lesson_19-queue/1-Shortener/pkg/api"
 	"thinknetica_golang_core/Lesson_19-queue/1-Shortener/pkg/queue"
+	"thinknetica_golang_core/Lesson_19-queue/1-Shortener/pkg/storage"
 
 	"github.com/gorilla/mux"
 )
@@ -17,15 +18,18 @@ const (
 
 func main() {
 	router := mux.NewRouter()
+	storage := storage.New()
 	q, err := queue.New(queueConnectionString, queueName)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	// обнуляем статистику при старте сервиса
+	q.PruneStat()
 	// Не забываем закрывать ресурсы
 	defer q.Close()
 
-	api := api.New(router, q)
+	api := api.New(router, q, storage)
 	api.Endpoints()
 	http.ListenAndServe(webAddr, router)
 }
